8947a5c192af8b4e6146b93e497dc28b700ca047
Utkarsh_patel <utkarshpatel@cedcoss.com>
2022-02-07 18:50:35 +0530

    product table

---
+++ src/product_table.js
@@ -0,0 +1,60 @@
+let list=[];
+
+function addProduct(){
+    var p_id=document.getElementById('p_id').value ;
+    var p_name=document.getElementById('p_name').value ;
+    var p_price=document.getElementById('p_price').value ;
+    
+  
+
+    if(checkDataType(p_id,p_name,p_price))
+    {
+        insertData(p_id,p_name,p_price);
+       document.getElementById('result').innerHTML="valid";
+        
+        display();
+       
+    }
+    else
+    {
+        document.getElementById('result').innerHTML="Invalid";
+    }
+
+}
+
+
+function checkDataType(p_id,p_name,p_price)
+{
+    if(p_id==""||isNaN(p_id)||p_name==""||p_price==""||isNaN(p_price))
+    return false;
+    return true;
+}
+
+function insertData(p_id,p_name,p_price)
+{
+    list.push({
+        "id":p_id,
+        "name":p_name,
+        "price":p_price
+    });
+}
+
+function display()
+{
+    let result="";
+    for(let i=0;i<list.length;i++)
+    {
+       
+        result+=`<table><tr><td style="padding:20px">${list[i].id}</td><td style="padding:20px">${list[i].name}</td><td style="padding:20px">${list[i].price}</td></tr></table>`;
+    }
+    document.getElementById('result').innerHTML=`<table >
+    <tr>
+    <th style="padding:20px">Product_id</th>
+    <th style="padding:20px">product_name</th>
+    <th style="padding:20px">product_price</th>
+    </tr>
+    <tr>
+    ${result}
+    </tr>
+    </table>`;
+}
